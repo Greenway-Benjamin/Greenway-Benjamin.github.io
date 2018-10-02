4c26de2ae4ae0a0f9e68416524438a4bbdefd66b
Greenway-Benjamin <benjamingreenway@gmail.com>
2018-10-02 16:55:35 +0000

    Update transitions.js

--- animationtrig/transitions.js
+++ animationtrig/transitions.js
@@ -16,6 +16,5 @@ for (var i = 0; i < frowneys.length; i++) {
       this.innerHTML = ':(';
       this.classList.remove('smiley1');
       this.classList.remove('smiley2');
-      this.classList.remove('smiley3'); 
-      alert('Alert!'); }, 5000);
+      this.classList.remove('smiley3'); }, 5000);
   }
