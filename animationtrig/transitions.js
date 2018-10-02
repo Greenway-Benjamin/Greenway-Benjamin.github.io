08ac0e4a025b205f113830fed4b86ed652beac5e
Greenway-Benjamin <benjamingreenway@gmail.com>
2018-10-02 13:56:04 +0000

    Create transitions.js

---
+++ animationtrig/transitions.js
@@ -0,0 +1,9 @@
+var frowneys = document.getElementsByClassName('frowneysmiley');
+
+for (i in frowneys) {
+  frowneys[i].addEventListener("mouseover", changeToSmiley());
+}
+
+function changeToSmiley() {
+  this.innerHTML(':)');
+}
