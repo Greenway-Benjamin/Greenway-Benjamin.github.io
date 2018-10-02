82a8303de0886b3e124baa7049590b32c5a711f8
Greenway-Benjamin <benjamingreenway@gmail.com>
2018-10-02 15:54:13 +0000

    Update transitions.js

--- animationtrig/transitions.js
+++ animationtrig/transitions.js
@@ -2,7 +2,7 @@ var frowneys = document.getElementsByClassName('frowneysmiley');
 
 for (var i = 0; i < frowneys.length; i++) {
   frowneys[i].addEventListener("mouseover", changeToSmiley);
-  
+}
   function changeToSmiley() {
     this.innerHTML = ':)';
   //  this.classList.add('smiley');
