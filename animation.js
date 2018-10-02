f83d1e61adb64803284c2a94ecb1ff44072f10e6
Greenway-Benjamin <benjamingreenway@gmail.com>
2018-10-01 19:53:04 -0600

    js

---
+++ animation.js
@@ -0,0 +1,15 @@
+var box1 = getElementById('box1');
+
+document.getElementById('box1button')
+onclick = function() {
+  if(this.innerHTML == 'Play') {
+    this.innerHTML = 'Pause';
+    box1.classList.add('boxanimation');
+  } else {
+    this.innerHTML = 'Play';
+    var computedStyle = window.getComputedStyle(box1);
+    var marginLeft = computedStyle.getPropertyValue('margin-left');
+    box1.style.marginLeft = marginLeft;
+    box1.classList.remove('boxanimation');
+  }
+}
