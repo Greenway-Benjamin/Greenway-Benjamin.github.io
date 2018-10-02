b3ee817f200873969a3b8e74301dbefda27bfd51
Greenway-Benjamin <benjamingreenway@gmail.com>
2018-10-01 21:07:02 -0600

    a

--- animation.js
+++ animation.js
@@ -3,10 +3,10 @@ var box = document.getElementById('box1');
 document.getElementById('box1button').onclick = function() {
   if(this.innerHTML == 'Play') {
     this.innerHTML = 'Pause';
-    box.style.animationplaystate = 'running';
+    box.style.animationPlayState = 'running';
   } else {
     this.innerHTML = 'Play';
-    box.style.animationplaystate = 'paused';
+    box.style.animationPlayState = 'paused';
     box.classList.remove('boxanimation');
   }
 }
