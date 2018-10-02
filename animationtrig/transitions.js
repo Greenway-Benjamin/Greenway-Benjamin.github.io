6cfd60a672a000bf720a440ce0328fafb1d675cc
Greenway-Benjamin <benjamingreenway@gmail.com>
2018-10-02 16:27:01 +0000

    Update transitions.js

--- animationtrig/transitions.js
+++ animationtrig/transitions.js
@@ -2,10 +2,18 @@ var frowneys = document.getElementsByClassName('frowneysmiley');
 
 for (var i = 0; i < frowneys.length; i++) {
   frowneys[i].addEventListener("mouseover", changeToSmiley);
+  frowneys[i].addEventListener("mouseout", changeBack)
 }
   function changeToSmiley() {
     var anum = Math.floor(Math.random() * 3) + 1  
     var newclass = 'smiley' + anum;
     this.innerHTML = ':)';
     this.classList.add(newclass);
+  }
+
+  function changeBack() {
+    setTimeOut(this.innerHTML = ':(';, 5000);
+    this.classList.remove('smiley1');
+    this.classList.remove('smiley2');
+    this.classList.remove('smiley3');
   }
