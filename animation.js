e2e3528ca361332f794bed1a6b73ba0b6a3ee05d
Greenway-Benjamin <benjamingreenway@gmail.com>
2018-10-01 20:46:29 -0600

    a

--- animation.js
+++ animation.js
@@ -9,8 +9,10 @@ document.getElementById('box1button').onclick = function() {
     var computedStyle = window.getComputedStyle(box);
     var marginLeft = computedStyle.getPropertyValue('left');
     var marginTop = computedStyle.getPropertyValue('top');
+    var rotated = computedStyle.getPropertyValue('rotation');
     box.style.left = marginLeft;
     box.style.top = marginTop;
+    box.style.rotation = rotated;
     box.classList.remove('boxanimation');
   }
 }
