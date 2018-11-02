a5f545952acb45e070b7a3ecd2003ffaf6257f00
Greenway-Benjamin <benjamingreenway@gmail.com>
2018-11-01 18:03:17 -0600

    Update modules/navbar.php

--- modules/navbar.php
+++ modules/navbar.php
@@ -3,4 +3,5 @@
         <li><a href="animationtrig/animations.html">Animations</a></li>
         <li><a href="animationtrig/transitions.html">Transitions</a></li>
         <li><a href="javascriptbasics/basics.html">Javascript Basics</a></li>
+        <li><a href="dommanipulation/dom.html">DOM Manipulation</a></li>
     </ul>
