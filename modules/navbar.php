fbd77f8050594583f50cd6619e953adbb228b4ee
Greenway-Benjamin <benjamingreenway@gmail.com>
2018-11-01 18:07:57 -0600

    a

--- modules/navbar.php
+++
@@ -1,7 +0,0 @@
-<ul id="navbar">
-        <li><a href="/">Home</a></li>
-        <li><a href="animationtrig/animations.html">Animations</a></li>
-        <li><a href="animationtrig/transitions.html">Transitions</a></li>
-        <li><a href="javascriptbasics/basics.html">Javascript Basics</a></li>
-        <li><a href="dommanipulation/dom.html">DOM Manipulation</a></li>
-    </ul>
